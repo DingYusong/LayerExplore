1dca225bc1adaa67cc85e9f8e67d608890ad2ce4
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-11-14 14:52:36 +0800

    Initial Commit

---
+++ LayerExplore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayerExplore
+//
+//  Created by 丁玉松 on 2018/11/14.
+//  Copyright © 2018 丁玉松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
